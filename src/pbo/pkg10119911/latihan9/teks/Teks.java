c59dcfa5add5cd61f511f6d98b15e5e379b13417
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 08:48:23 +0700

    Teks

---
+++ src/pbo/pkg10119911/latihan9/teks/Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10119911.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mochamad Tegar Dwi S
+ * KELAS    : IF10K
+ * NIM      : 10119911 
+ * Deskripsi Program : Teks
+ */
+public class Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "char:" + tab + "berisi" + c);
+    }
+    
+}
